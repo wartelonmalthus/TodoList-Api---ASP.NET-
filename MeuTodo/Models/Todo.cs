f54b97d33e70b531de5ea878e55aa225c7428edb
Wartelon   Malthus <wartelon.malthus@gmail.com>
2021-10-25 22:31:36 -0300

    Fazendo conexão com o banco de dados Sqlite, configurando o dbcontext

---
+++ MeuTodo/Models/Todo.cs
@@ -0,0 +1,16 @@
+namespace MeuTodo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; } 
+
+        public string Title { get; set; }
+
+        public bool Done { get; set; }  
+
+
+
+        
+    }
+
+}
